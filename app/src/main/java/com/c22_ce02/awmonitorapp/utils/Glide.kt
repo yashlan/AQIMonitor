86bb6ef34ed5db3086d089f728cbd34d1d56d8de
yashlan <yashlan007@gmail.com>
2022-05-11 14:16:24 +0700

    Create Glide.kt

---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/Glide.kt
@@ -0,0 +1,27 @@
+package com.c22_ce02.awmonitorapp.utils
+
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+fun AppCompatActivity.loadImageViaGlide(uri: Uri?, target: ImageView) {
+    Glide
+        .with(this)
+        .load(uri)
+        .centerCrop()
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+        .into(target)
+}
+
+fun AppCompatActivity.loadImageViaGlide(drawable: Int, target: ImageView) {
+    Glide
+        .with(this)
+        .load(ContextCompat.getDrawable(this, drawable))
+        .centerCrop()
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+        .into(target)
+}
