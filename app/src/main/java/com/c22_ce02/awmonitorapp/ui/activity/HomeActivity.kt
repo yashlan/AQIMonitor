5e58ab64f1172bd79faae970f89ca2572b0862ef
yashlan <yashlan007@gmail.com>
2022-05-11 14:17:29 +0700

    Update HomeActivity.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/HomeActivity.kt
@@ -1,10 +1,15 @@
 package com.c22_ce02.awmonitorapp.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.c22_ce02.awmonitorapp.R
 import com.c22_ce02.awmonitorapp.databinding.ActivityHomeBinding
+import com.c22_ce02.awmonitorapp.utils.loadImageViaGlide
+import com.c22_ce02.awmonitorapp.utils.showToast
 import com.c22_ce02.awmonitorapp.utils.viewBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
 
@@ -13,7 +18,36 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.tvHello.text = getString(R.string.app_name)
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }else {
+            binding.tvName.text = "nama : " + user.displayName
+            binding.tvEmail.text = "email : " + user.email
+            loadImageViaGlide(user.photoUrl, binding.imgPhoto)
+        }
 
+        binding.btnLogout.setOnClickListener {
+            logout()
+        }
+    }
+
+    override fun onBackPressed() {
+        finishAffinity()
+    }
+    
+    private fun logout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }
+                else {
+                    showToast(task.result.toString())
+                }
+            }
     }
 }
