a88639fda6e212673a4819828851aefe2b820c7b
yashlan <yashlan007@gmail.com>
2022-05-17 13:27:25 +0700

    Update ApiConfig.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/api/ApiConfig.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/api/ApiConfig.kt
@@ -1,4 +1,40 @@
 package com.c22_ce02.awmonitorapp.api
 
+import com.c22_ce02.awmonitorapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ApiConfig {
+
+    companion object {
+
+        fun getApiService(baseUrl: String): ApiService {
+
+            val loggingInterceptor = if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            else
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+
+            val client = OkHttpClient
+                .Builder()
+                .addInterceptor { chain ->
+                    val newRequest = chain.request()
+                        .newBuilder()
+                        .build()
+                    chain.proceed(newRequest)
+                }
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseUrl)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 }
