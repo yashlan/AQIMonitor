73cfc29e27476dae49b0de9525103bc5c01320c0
yashlan <yashlan007@gmail.com>
2022-05-17 13:28:07 +0700

    Create CurrentAirQualityRepository.kt

---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/data/repository/CurrentAirQualityRepository.kt
@@ -0,0 +1,16 @@
+package com.c22_ce02.awmonitorapp.data.repository
+
+import com.c22_ce02.awmonitorapp.api.ApiService
+
+class CurrentAirQualityRepository(private val apiService: ApiService) {
+    fun getCurrentAirQuality(
+        lat: Double,
+        lon: Double,
+        apiKey: String
+    ) =
+        apiService.getCurrentAirQuality(
+            lat,
+            lon,
+            apiKey
+        )
+}
