ef8328b80dc0742032e4fb05e32f2690576f67fc
yashlan <yashlan007@gmail.com>
2022-05-18 22:18:50 +0700

    change binding to delegate, loadImageViaGlide

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/splash/SplashActivity.kt
@@ -6,30 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.c22_ce02.awmonitorapp.R
 import com.c22_ce02.awmonitorapp.databinding.ActivitySplashBinding
 import com.c22_ce02.awmonitorapp.data.preference.CheckHelper
 import com.c22_ce02.awmonitorapp.data.preference.CheckPreference
 import com.c22_ce02.awmonitorapp.ui.activity.GetStartedActivity
 import com.c22_ce02.awmonitorapp.ui.activity.HomeActivity
+import com.c22_ce02.awmonitorapp.utils.loadImageViaGlide
 import com.c22_ce02.awmonitorapp.utils.setFullscreen
+import com.c22_ce02.awmonitorapp.utils.viewBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
+    private val binding by viewBinding<ActivitySplashBinding>()
 
     private lateinit var mCheckPreferences: CheckPreference
     private lateinit var checkHelper: CheckHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         setFullscreen()
-
+        super.onCreate(savedInstanceState)
+        loadImageViaGlide(R.drawable.logo_no_background, binding.imageView)
         mCheckPreferences = CheckPreference(this)
         checkHelper = mCheckPreferences.getCheck()
-
         Handler(Looper.getMainLooper()).postDelayed({
             if (checkHelper.isLogin) {
                 startActivity(Intent(this, HomeActivity::class.java))
