4e5c8b6868c070ec7e0099941bfc2ce7dd1975a5
yashlan <yashlan007@gmail.com>
2022-05-18 18:34:20 +0700

    Update HomeActivity.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/HomeActivity.kt
@@ -1,137 +1,91 @@
 package com.c22_ce02.awmonitorapp.ui.activity
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.os.Handler
+import android.os.Looper
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.c22_ce02.awmonitorapp.BuildConfig
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.c22_ce02.awmonitorapp.R
 import com.c22_ce02.awmonitorapp.databinding.ActivityHomeBinding
-import com.c22_ce02.awmonitorapp.ui.view.model.CurrentAirQualityViewModel
-import com.c22_ce02.awmonitorapp.ui.view.model.CurrentConditionViewModel
-import com.c22_ce02.awmonitorapp.ui.view.model.FiveDaysOfDailyForecastViewModel
-import com.c22_ce02.awmonitorapp.ui.view.model.GeoPositionViewModel
-import com.c22_ce02.awmonitorapp.ui.view.modelfactory.CurrentAirQualityViewModelFactory
-import com.c22_ce02.awmonitorapp.ui.view.modelfactory.CurrentConditionViewModelFactory
-import com.c22_ce02.awmonitorapp.ui.view.modelfactory.FiveDaysOfDailyForecastViewModelFactory
-import com.c22_ce02.awmonitorapp.ui.view.modelfactory.GeoPositionViewModelFactory
+import com.c22_ce02.awmonitorapp.utils.setFullscreen
 import com.c22_ce02.awmonitorapp.utils.showToast
 import com.c22_ce02.awmonitorapp.utils.viewBinding
 
+
 class HomeActivity : AppCompatActivity() {
 
+    private var canExit = false
+    private lateinit var navController: NavController
     private val binding by viewBinding<ActivityHomeBinding>()
-    private val geoPositionViewModel: GeoPositionViewModel by viewModels {
-        GeoPositionViewModelFactory()
-    }
-    private val fiveDaysOfDailyForecastViewModel: FiveDaysOfDailyForecastViewModel by viewModels {
-        FiveDaysOfDailyForecastViewModelFactory()
-    }
-    private val currentConditionViewModel: CurrentConditionViewModel by viewModels {
-        CurrentConditionViewModelFactory()
-    }
-    private val currentAirQualityViewModel: CurrentAirQualityViewModel by viewModels {
-        CurrentAirQualityViewModelFactory()
-    }
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
+        setFullscreen()
         super.onCreate(savedInstanceState)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHostFragmentActivityHomeActivity.id)
+        navHostFragment?.findNavController()?.let {
+            navController = it
+        }
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_discover,
+                R.id.navigation_glossary,
+            )
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+        removeItemTintColor()
+    }
 
-        currentAirQualityViewModel.getCurrentAirQuality(
-            -5.299,
-            104.98,
-            BuildConfig.API_KEY_WEATHERBIT,
-            onSuccess = {
-                showToast(it?.data?.size.toString())
-                val data = it?.data?.get(0)
-                binding.tvAirQualityToday.text =
-                    "AQI = ${data?.aqi}\n" +
-                            "O3 = ${data?.o3}\n" +
-                            "SO2 = ${data?.so2}\n" +
-                            "NO2 = ${data?.no2}\n" +
-                            "CO = ${data?.co}\n" +
-                            "PM10 = ${data?.pm10}\n" +
-                            "PM25 = ${data?.pm25}\n"
-            },
-            onFailed = { errorMsg ->
-                if (errorMsg != null) {
-                    showToast(errorMsg)
-                }
-            }
-        )
-
-        geoPositionViewModel.getInformationUserByGeoPosition(
-            "-5.299,104.98",
-            onSuccess = { r ->
-                binding.tvMyLocation.text =
-                    "Lokasi Saya : ${r?.localizedName}, " +
-                            "${r?.administrativeArea?.localizedName}, " +
-                            "${r?.country?.localizedName}"
-
-                if (r?.locationKey != null) {
-                    currentConditionViewModel.getCurrentCondition(
-                        r.locationKey,
-                        onSuccess = {
-                            val tempC =
-                                "${it?.get(0)?.temperature?.metric?.value}${it?.get(0)?.temperature?.metric?.unit}"
-                            binding.tvCurrentCondition.text =
-                                "Cuaca Saat Ini : ${it?.get(0)?.weatherText}, $tempC"
-                        },
-                        onFailed = { errorMsg ->
-                            if (errorMsg != null) {
-                                showToast("currentConditionError : $errorMsg")
-                            }
-                        }
-                    )
-
-                    fiveDaysOfDailyForecastViewModel.get5DaysOfDailyForecasts(
-                        r.locationKey,
-                        onSuccess = {
-                            val f0 =
-                                "${it?.dailyForecasts?.get(0)?.temperature?.minimum?.value}F - ${
-                                    it?.dailyForecasts?.get(0)?.temperature?.maximum?.value
-                                }F"
-                            val f1 =
-                                "${it?.dailyForecasts?.get(1)?.temperature?.minimum?.value}F - ${
-                                    it?.dailyForecasts?.get(1)?.temperature?.maximum?.value
-                                }F"
-                            val f2 =
-                                "${it?.dailyForecasts?.get(2)?.temperature?.minimum?.value}F - ${
-                                    it?.dailyForecasts?.get(2)?.temperature?.maximum?.value
-                                }F"
-                            val f3 =
-                                "${it?.dailyForecasts?.get(3)?.temperature?.minimum?.value}F - ${
-                                    it?.dailyForecasts?.get(3)?.temperature?.maximum?.value
-                                }F"
-                            val f4 =
-                                "${it?.dailyForecasts?.get(4)?.temperature?.minimum?.value}F - ${
-                                    it?.dailyForecasts?.get(4)?.temperature?.maximum?.value
-                                }F"
-
-                            with(binding) {
-                                tvForecastDay1.text = "Perkiraan Suhu : $f0"
-                                tvForecastDay2.text = "Perkiraan Suhu : $f1"
-                                tvForecastDay3.text = "Perkiraan Suhu : $f2"
-                                tvForecastDay4.text = "Perkiraan Suhu : $f3"
-                                tvForecastDay5.text = "Perkiraan Suhu : $f4"
-                            }
-                        },
-                        onFailed = { errorMsg ->
-                            if (errorMsg != null) {
-                                showToast(errorMsg)
-                            }
-                        }
-                    )
-                }
-            },
-            onFailed = { errorMsg ->
-                if (errorMsg != null) {
-                    showToast(errorMsg)
-                }
-            })
+    private fun removeItemTintColor() {
+        binding.navView.itemIconTintList = null
     }
 
     override fun onBackPressed() {
-        finishAffinity()
+        if (canExit) {
+            super.onBackPressed()
+            finishAffinity()
+            return
+        }
+        canExit = true
+        showToast(R.string.exit_info)
+        Handler(Looper.getMainLooper()).postDelayed({
+            canExit = false
+        }, 2000)
+    }
+
+    fun onClickItemNavHome(item: MenuItem) {
+        navController.navigate(R.id.navigation_home)
+        changeStateIconItem(item)
+    }
+
+    fun onClickItemNavDiscover(item: MenuItem) {
+        navController.navigate(R.id.navigation_discover)
+        changeStateIconItem(item)
+    }
+
+    fun onClickItemNavGlossary(item: MenuItem) {
+        navController.navigate(R.id.navigation_glossary)
+        changeStateIconItem(item)
+    }
+
+    private fun changeStateIconItem(menuItem: MenuItem) {
+        when (menuItem.itemId) {
+            R.id.item_navigation_home -> {
+                menuItem.isChecked = true
+            }
+            R.id.item_navigation_discover -> {
+                menuItem.isChecked = true
+            }
+            R.id.item_navigation_glossary -> {
+                menuItem.isChecked = true
+            }
+        }
     }
 }
