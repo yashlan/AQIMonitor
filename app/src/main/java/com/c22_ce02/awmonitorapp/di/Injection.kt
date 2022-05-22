c041a411c29fed965c0499fefd0ff45c2a4d10ce
yashlan <yashlan007@gmail.com>
2022-05-22 16:21:09 +0700

    Update Injection.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/di/Injection.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/di/Injection.kt
@@ -2,29 +2,22 @@ package com.c22_ce02.awmonitorapp.di
 
 import com.c22_ce02.awmonitorapp.BuildConfig
 import com.c22_ce02.awmonitorapp.api.ApiConfig
-import com.c22_ce02.awmonitorapp.data.repository.CurrentAirQualityRepository
-import com.c22_ce02.awmonitorapp.data.repository.CurrentConditionRepository
-import com.c22_ce02.awmonitorapp.data.repository.FiveDaysOfDailyForecastRepository
-import com.c22_ce02.awmonitorapp.data.repository.GeoPositionRepository
+import com.c22_ce02.awmonitorapp.data.repository.*
 
 object Injection {
-    fun provideGeoPositionRepository() : GeoPositionRepository {
-        val apiService = ApiConfig.getApiService(BuildConfig.BASE_URL_ACCUWEATHER)
-        return GeoPositionRepository(apiService)
-    }
 
-    fun provideFiveDaysOfDailyForecastRepository() : FiveDaysOfDailyForecastRepository {
-        val apiService = ApiConfig.getApiService(BuildConfig.BASE_URL_ACCUWEATHER)
-        return FiveDaysOfDailyForecastRepository(apiService)
-    }
-
-    fun provideCurrentConditionRepository() : CurrentConditionRepository {
-        val apiService = ApiConfig.getApiService(BuildConfig.BASE_URL_ACCUWEATHER)
-        return CurrentConditionRepository(apiService)
+    fun provideCurrentWeatherConditionRepository() : CurrentWeatherConditionRepository {
+        val apiService = ApiConfig.getApiService(BuildConfig.BASE_URL_WEATHERBIT)
+        return CurrentWeatherConditionRepository(apiService)
     }
 
     fun provideCurrentAirQualityRepository() : CurrentAirQualityRepository {
         val apiService = ApiConfig.getApiService(BuildConfig.BASE_URL_WEATHERBIT)
         return CurrentAirQualityRepository(apiService)
+    }
+
+    fun provideEveryHourAirQualityForecastRepository(): AirQualityForecastByHourRepository {
+        val apiService = ApiConfig.getApiService(BuildConfig.BASE_URL_WEATHERBIT)
+        return AirQualityForecastByHourRepository(apiService)
     }
 }
