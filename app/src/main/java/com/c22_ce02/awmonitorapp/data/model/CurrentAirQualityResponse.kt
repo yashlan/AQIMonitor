b6565646e2517a54c987787821a20baf93df49e0
yashlan <yashlan007@gmail.com>
2022-05-22 16:20:44 +0700

    Update CurrentAirQualityResponse.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/data/model/CurrentAirQualityResponse.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/data/model/CurrentAirQualityResponse.kt
@@ -3,24 +3,12 @@ package com.c22_ce02.awmonitorapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class CurrentAirQualityResponse(
-    @SerializedName("lat")
-    val lat: Double,
-    @SerializedName("lon")
-    val lon: Double,
-    @SerializedName("timezone")
-    val timezone: String,
-    @SerializedName("city_name")
-    val cityName: String,
-    @SerializedName("country_code")
-    val countryCode: String,
-    @SerializedName("state_code")
-    val stateCode: String,
     @SerializedName("data")
     val data: List<Data>
 ) {
     data class Data(
         @SerializedName("aqi")
-        val aqi: Int,
+        val aqi: Double,
         @SerializedName("o3")
         val o3: Double,
         @SerializedName("so2")
@@ -33,15 +21,5 @@ data class CurrentAirQualityResponse(
         val pm10: Double,
         @SerializedName("pm25")
         val pm25: Double,
-        @SerializedName("pollen_level_tree")
-        val pollenLevelTree: Int,
-        @SerializedName("pollen_level_grass")
-        val pollenLevelGrass: Int,
-        @SerializedName("pollen_level_weed")
-        val pollenLevelWeed: Int,
-        @SerializedName("mold_level")
-        val moldLevel: Int,
-        @SerializedName("predominant_pollen_type")
-        val predominantPollenType: String
     )
 }
