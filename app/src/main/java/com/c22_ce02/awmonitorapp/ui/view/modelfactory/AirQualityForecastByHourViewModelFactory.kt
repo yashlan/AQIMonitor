5e371acd860ffcc793dcfe1fa80fe75aa07f027f
yashlan <yashlan007@gmail.com>
2022-05-22 16:21:30 +0700

    Create AirQualityForecastByHourViewModelFactory.kt

---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/view/modelfactory/AirQualityForecastByHourViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.c22_ce02.awmonitorapp.ui.view.modelfactory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.c22_ce02.awmonitorapp.di.Injection
+import com.c22_ce02.awmonitorapp.ui.view.model.AirQualityForecastByHourViewModel
+
+@Suppress("UNCHECKED_CAST")
+class AirQualityForecastByHourViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(AirQualityForecastByHourViewModel::class.java)) {
+            AirQualityForecastByHourViewModel(Injection.provideEveryHourAirQualityForecastRepository()) as T
+        } else {
+            throw IllegalArgumentException("ViewModel of ${modelClass.simpleName} Not Found")
+        }
+    }
+
+}
