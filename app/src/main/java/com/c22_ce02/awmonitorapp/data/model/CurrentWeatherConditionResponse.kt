85094320382e11952810ba5199e04a5114f06b46
yashlan <yashlan007@gmail.com>
2022-05-22 16:20:50 +0700

    Create CurrentWeatherConditionResponse.kt

---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/data/model/CurrentWeatherConditionResponse.kt
@@ -0,0 +1,19 @@
+package com.c22_ce02.awmonitorapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentWeatherConditionResponse(
+    @SerializedName("data")
+    val data: List<Data>,
+) {
+    data class Data(
+        @SerializedName("aqi")
+        val aqi: Double,
+        @SerializedName("rh")
+        val humidity: Double,
+        @SerializedName("wind_spd")
+        val windSpeed: Double,
+        @SerializedName("temp")
+        val temperature: Double,
+    )
+}
