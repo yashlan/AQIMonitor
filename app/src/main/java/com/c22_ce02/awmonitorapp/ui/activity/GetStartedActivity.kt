da779f6dece5c42fe4de2b750a4eaa093e538422
yashlan <yashlan007@gmail.com>
2022-05-24 23:28:16 +0700

    Update GetStartedActivity.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/GetStartedActivity.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/GetStartedActivity.kt
@@ -3,18 +3,21 @@ package com.c22_ce02.awmonitorapp.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.c22_ce02.awmonitorapp.R
 import com.c22_ce02.awmonitorapp.databinding.ActivityGetStartedBinding
 import com.c22_ce02.awmonitorapp.data.preference.CheckHelper
 import com.c22_ce02.awmonitorapp.data.preference.CheckPreference
+import com.c22_ce02.awmonitorapp.utils.setFullscreen
 import com.c22_ce02.awmonitorapp.utils.showToast
-import com.c22_ce02.awmonitorapp.utils.viewBinding
 
-class GetStartedActivity : AppCompatActivity() {
+class GetStartedActivity : AppCompatActivity(R.layout.activity_get_started) {
 
-    private val binding by viewBinding<ActivityGetStartedBinding>()
+    private val binding by viewBinding(ActivityGetStartedBinding::bind)
     private lateinit var checkHelper: CheckHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setFullscreen()
         super.onCreate(savedInstanceState)
         this.title = "Get Started"
         checkHelper = CheckHelper()
