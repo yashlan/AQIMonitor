10ceeab727fddcb6e3fb3a11013ea17622063ae8
yashlan <yashlan007@gmail.com>
2022-05-26 13:58:57 +0700

    Update AirQualityForecastByHour.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/data/model/AirQualityForecastByHour.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/data/model/AirQualityForecastByHour.kt
@@ -1,8 +1,18 @@
 package com.c22_ce02.awmonitorapp.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+
+@Parcelize
 data class AirQualityForecastByHour(
     val hour: String,
     val iconAQISrc: Int,
-    val aqi: Int
-)
+    val aqi: Int,
+    val pm10: Int,
+    val pm25: Int,
+    val o3: Int,
+    val so2: Int,
+    val no2: Int,
+    val co: Int
+) : Parcelable
