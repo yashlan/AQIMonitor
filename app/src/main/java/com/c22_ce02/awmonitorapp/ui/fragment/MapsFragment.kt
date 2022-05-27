f8e23dd7b7e17a0a1c1befc30a7fe2fb09a2ae55
yashlan <yashlan007@gmail.com>
2022-05-27 11:24:50 +0700

    Update MapsFragment.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/fragment/MapsFragment.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/fragment/MapsFragment.kt
@@ -1,14 +1,12 @@
 package com.c22_ce02.awmonitorapp.ui.fragment
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.c22_ce02.awmonitorapp.R
 import com.c22_ce02.awmonitorapp.databinding.FragmentMapsBinding
 import com.c22_ce02.awmonitorapp.ui.view.model.MapsViewModel
@@ -19,8 +17,8 @@ import com.google.android.gms.maps.SupportMapFragment
 class MapsFragment : Fragment(R.layout.fragment_maps) {
 
 
-    private val mapsViewModel : MapsViewModel by viewModels()
-    private lateinit var binding : FragmentMapsBinding
+    private val mapsViewModel: MapsViewModel by viewModels()
+    private val binding by viewBinding(FragmentMapsBinding::bind)
 
 
     private val callback = OnMapReadyCallback { googleMap ->
@@ -29,30 +27,21 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
         Handler(Looper.getMainLooper()).postDelayed({
             showLoading(false)
         }, MAPS_FAKE_TIME_LOAD)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentMapsBinding.inflate(inflater,container,false)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-
     }
 
     private fun showLoading(isLoading: Boolean) {
         if (isLoading) {
             binding.mapsLoading.visibility = View.VISIBLE
+            binding.map.visibility = View.GONE
         } else {
             binding.mapsLoading.visibility = View.GONE
+            binding.map.visibility = View.VISIBLE
         }
     }
 
