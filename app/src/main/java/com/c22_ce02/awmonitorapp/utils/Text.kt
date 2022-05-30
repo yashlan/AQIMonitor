727538716b176d80c6ae517943ab148e95684a09
yashlan <yashlan007@gmail.com>
2022-05-30 11:23:35 +0700

    Update Text.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/Text.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/Text.kt
@@ -5,20 +5,18 @@ import android.text.Spanned
 import android.text.style.RelativeSizeSpan
 
 
-object Text {
-    fun spannableStringBuilder(
-        text: String,
-        startChar: Char,
-        reduceBy: Float
-    ): SpannableStringBuilder {
-        val smallSizeText = RelativeSizeSpan(reduceBy)
-        val ssBuilder = SpannableStringBuilder(text)
-        ssBuilder.setSpan(
-            smallSizeText,
-            text.indexOf(startChar),
-            text.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        return ssBuilder
-    }
+fun spannableStringBuilder(
+    text: String,
+    startChar: Char,
+    reduceBy: Float
+): SpannableStringBuilder {
+    val smallSizeText = RelativeSizeSpan(reduceBy)
+    val ssBuilder = SpannableStringBuilder(text)
+    ssBuilder.setSpan(
+        smallSizeText,
+        text.indexOf(startChar),
+        text.length,
+        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    return ssBuilder
 }
