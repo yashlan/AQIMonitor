1fa3ee45d5d4a4c27502e097ee9b70866e6c4c77
rizkysiregar <regarrezky@gmail.com>
2022-05-31 12:14:31 +0700

    add onboarding activity and fragment slide

---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/fragment/FirstSlideFragment.kt
@@ -0,0 +1,47 @@
+package com.c22_ce02.awmonitorapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.c22_ce02.awmonitorapp.R
+import com.c22_ce02.awmonitorapp.databinding.FragmentFirstSlideBinding
+
+
+class FirstSlideFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var binding : FragmentFirstSlideBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstSlideBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnStarted = binding.btnStarted
+        btnStarted.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_started){
+            val mSecondSlideFragment = SecondSlideFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in,
+                    R.anim.fade_out
+                )
+                replace(R.id.frame_container,mSecondSlideFragment,SecondSlideFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/fragment/SecondSlideFragment.kt
@@ -0,0 +1,73 @@
+package com.c22_ce02.awmonitorapp.ui.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.c22_ce02.awmonitorapp.BuildConfig
+import com.c22_ce02.awmonitorapp.R
+import com.c22_ce02.awmonitorapp.data.preference.CheckHelper
+import com.c22_ce02.awmonitorapp.data.preference.CheckPreference
+import com.c22_ce02.awmonitorapp.databinding.FragmentSecondSlideBinding
+import com.c22_ce02.awmonitorapp.ui.activity.HomeActivity
+import com.c22_ce02.awmonitorapp.utils.showToast
+
+class SecondSlideFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var binding : FragmentSecondSlideBinding
+    private lateinit var checkHelper: CheckHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkHelper = CheckHelper()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondSlideBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnNext = binding.btnNext
+        val btnSkip = binding.secondBtnSkip
+
+        btnNext.setOnClickListener(this)
+        btnSkip.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_next){
+            val mThirdSlideFragment = ThirdSlideFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in,
+                    R.anim.fade_out
+                )
+                replace(R.id.frame_container,mThirdSlideFragment,ThirdSlideFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }else if(v?.id == R.id.second_btn_skip){
+                val intent = Intent(requireContext(),HomeActivity::class.java)
+                startActivity(intent)
+                savePref()
+                activity?.finish()
+        }
+    }
+
+    private fun savePref() {
+        val checkPreference = CheckPreference(requireContext())
+        checkHelper.isLogin = true
+        checkPreference.setCheck(checkHelper)
+        if (BuildConfig.DEBUG) {
+            showToast("Selamat Datang")
+        }
+    }
+}
