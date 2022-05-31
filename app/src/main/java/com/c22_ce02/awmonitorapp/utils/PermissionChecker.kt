0670d6a2c54feba0d00256408e0167701300b6de
yashlan <yashlan007@gmail.com>
2022-06-01 02:33:21 +0700

    Update PermissionChecker.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/PermissionChecker.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/PermissionChecker.kt
@@ -1,12 +1,20 @@
 package com.c22_ce02.awmonitorapp.utils
 
 import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 fun Fragment.isPermissionGranted(permission: String): Boolean {
     return ContextCompat.checkSelfPermission(
         requireContext(),
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
+}
+
+fun AppCompatActivity.isPermissionGranted(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
         permission
     ) == PackageManager.PERMISSION_GRANTED
 }
