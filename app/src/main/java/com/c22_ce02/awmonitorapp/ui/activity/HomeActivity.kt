a2359f9ecd5a14af1b2f10f06d4cb1041b109e9a
yashlan <yashlan007@gmail.com>
2022-06-01 23:28:34 +0700

    Update HomeActivity.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/activity/HomeActivity.kt
@@ -1,9 +1,16 @@
 package com.c22_ce02.awmonitorapp.ui.activity
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.net.Uri
+import android.os.*
+import android.provider.Settings
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -14,8 +21,7 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.c22_ce02.awmonitorapp.R
 import com.c22_ce02.awmonitorapp.databinding.ActivityHomeBinding
 import com.c22_ce02.awmonitorapp.notification.AirQualityNotificationReceiver
-import com.c22_ce02.awmonitorapp.utils.setFullscreen
-import com.c22_ce02.awmonitorapp.utils.showToast
+import com.c22_ce02.awmonitorapp.utils.*
 
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
@@ -24,7 +30,30 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     private lateinit var navController: NavController
     private val binding by viewBinding(ActivityHomeBinding::bind, R.id.container)
 
+    private val backgroundLocationPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+        { isGranted ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                when {
+                    isGranted -> {
+                        return@registerForActivityResult
+                    }
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION) -> {
+                        return@registerForActivityResult
+                    }
+                    else -> {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        forcePortraitScreenOrientation()
         setFullscreen()
         super.onCreate(savedInstanceState)
         val navHostFragment =
@@ -35,7 +64,7 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
         val appBarConfiguration = AppBarConfiguration.Builder(
             setOf(
                 R.id.navigation_home,
-                R.id.mapsFragment,
+                R.id.navigation_maps,
                 R.id.navigation_glossary,
             )
         ).build()
@@ -43,15 +72,54 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
         binding.navView.setupWithNavController(navController)
         removeItemTintColor()
         setupNotification()
+
+        if (isAllLocationGranted()) {
+            askIgnoreBatteryOptimization()
+            askAccessBackgroundLocation()
+        }
+
+    }
+
+    private fun isAllLocationGranted(): Boolean =
+        isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                isPermissionGranted(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    private fun askAccessBackgroundLocation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (!isPermissionGranted(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                createCustomAlertDialog(
+                    getString(R.string.konfirmasi),
+                    getString(R.string.permission_msg_access_bg_location),
+                    actionPositiveButton = {
+                        backgroundLocationPermission.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    }
+                )
+            }
+        }
+    }
+
+    @SuppressLint("BatteryLife")
+    private fun askIgnoreBatteryOptimization() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val pm = getSystemService(Context.POWER_SERVICE) as PowerManager
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                val intent = Intent()
+                intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+                intent.flags = FLAG_ACTIVITY_NEW_TASK
+                intent.data = Uri.parse("package:$packageName")
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun setupNotification() {
+        val receiver = AirQualityNotificationReceiver()
+        receiver.createChannel(this)
+        receiver.setRepeatingNotification(this)
     }
 
     private fun removeItemTintColor() {
         binding.navView.itemIconTintList = null
-    }
-
-    private fun setupNotification() {
-        showToast("test notif")
-        AirQualityNotificationReceiver().setRepeatingNotification(this)
     }
 
     override fun onBackPressed() {
@@ -76,10 +144,10 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     }
 
     fun onClickItemNavDiscover(item: MenuItem) {
-        if (navController.currentDestination?.id == R.id.mapsFragment) {
+        if (navController.currentDestination?.id == R.id.navigation_maps) {
             return
         }
-        navController.navigate(R.id.mapsFragment)
+        navController.navigate(R.id.navigation_maps)
         changeStateIconItem(item)
     }
 
