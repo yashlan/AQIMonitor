2a94a0ed72e71f419fe88f4c7c3fbec0d88734c0
yashlan <yashlan007@gmail.com>
2022-06-07 01:33:10 +0700

    Update Time.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/Time.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/Time.kt
@@ -1,11 +1,9 @@
 package com.c22_ce02.awmonitorapp.utils
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import net.time4j.PrettyTime
 import net.time4j.android.ApplicationStarter
 import net.time4j.format.expert.Iso8601Format
-import java.text.SimpleDateFormat
 import java.util.*
 
 fun Fragment.initializeTime4A() {
@@ -15,14 +13,4 @@ fun Fragment.initializeTime4A() {
 fun convertToTimeAgo(date: String): String {
     val moment = Iso8601Format.EXTENDED_DATE_TIME_OFFSET.parse(date)
     return PrettyTime.of(Locale("id")).printRelativeInStdTimezone(moment)
-}
-
-fun getCurrentTimeOf(date: String): Long {
-    val dateFormat = SimpleDateFormat(
-        "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
-        Locale.getDefault()
-    )
-    val pasTime = dateFormat.parse(date)
-    val nowTime = Date()
-    return nowTime.time - pasTime?.time!!
 }
