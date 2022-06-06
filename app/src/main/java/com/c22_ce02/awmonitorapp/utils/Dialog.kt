0e93ac98674aaaaf9b3b9146c9a27d9e2dffab22
yashlan <yashlan007@gmail.com>
2022-06-06 12:06:57 +0700

    Update Dialog.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/Dialog.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/Dialog.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import com.c22_ce02.awmonitorapp.R
 
 fun AppCompatActivity.createCustomAlertDialog(
@@ -19,6 +19,35 @@ fun AppCompatActivity.createCustomAlertDialog(
         .inflate(
             R.layout.dialog_custom,
             findViewById<CardView>(R.id.layoutDialogContainer)
+        )
+    builder.setView(view)
+    view.findViewById<TextView>(R.id.tvDialogTitle).text = title
+    view.findViewById<TextView>(R.id.tvDialogMessage).text = message
+
+    val alertDialog = builder.setCancelable(false).create()
+
+    view.findViewById<Button>(R.id.buttonDialogNo).setOnClickListener {
+        alertDialog.dismiss()
+    }
+
+    view.findViewById<Button>(R.id.buttonDialogYes).setOnClickListener {
+        actionPositiveButton.invoke()
+        alertDialog.dismiss()
+    }
+
+    alertDialog.show()
+}
+
+fun Fragment.createCustomAlertDialog(
+    title: String,
+    message: String,
+    actionPositiveButton: () -> Unit
+) {
+    val builder = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
+    val view = LayoutInflater.from(requireContext())
+        .inflate(
+            R.layout.dialog_custom,
+            requireActivity().findViewById<CardView>(R.id.layoutDialogContainer)
         )
     builder.setView(view)
     view.findViewById<TextView>(R.id.tvDialogTitle).text = title
