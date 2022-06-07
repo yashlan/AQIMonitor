6c377d7fca34f4d440d81d61d6160aac09120653
Habib Rizky A.Siregar <regarrezky@gmail.com>
2022-06-07 10:33:51 +0700

    create data class mapscurrrent response

---
+++ app/src/main/java/com/c22_ce02/awmonitorapp/data/response/MapsCurrentResponse.kt
@@ -0,0 +1,46 @@
+package com.c22_ce02.awmonitorapp.data.response
+import com.google.gson.annotations.SerializedName
+
+data class MapsCurrentResponse(
+    @field:SerializedName("data")
+    val data: Data
+)
+
+data class CurrentItem(
+
+    @field:SerializedName("no2")
+    val no2: Double,
+
+    @field:SerializedName("o3")
+    val o3: Double,
+
+    @field:SerializedName("pm25")
+    val pm25: Double,
+
+    @field:SerializedName("city")
+    val city: String,
+
+    @field:SerializedName("so2")
+    val so2: Double,
+
+    @field:SerializedName("aqi")
+    val aqi: Int,
+
+    @field:SerializedName("pm10")
+    val pm10: Double,
+
+    @field:SerializedName("lon")
+    val lon: Double,
+
+    @field:SerializedName("co")
+    val co: Double,
+
+    @field:SerializedName("lat")
+    val lat: Double
+)
+
+data class Data(
+
+    @field:SerializedName("current")
+    val current: List<CurrentItem>
+)
