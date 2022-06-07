0ed826da065918f14d51ee8c24afa2c5fbdbe6b4
yashlan <yashlan007@gmail.com>
2022-06-07 20:47:29 +0700

    Update ArticleViewModel.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/ui/view/model/ArticleViewModel.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/ui/view/model/ArticleViewModel.kt
@@ -1,36 +1,34 @@
 package com.c22_ce02.awmonitorapp.ui.view.model
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.c22_ce02.awmonitorapp.BuildConfig
 import com.c22_ce02.awmonitorapp.data.response.ArticleResponse
 import com.c22_ce02.awmonitorapp.data.repository.ArticleRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class ArticleViewModel(private val repository: ArticleRepository): ViewModel() {
+class ArticleViewModel(private val repository: ArticleRepository) : ViewModel() {
 
-    val listArticle = MutableLiveData<List<ArticleResponse.ItemsItem>?>()
-    val errorMessage = MutableLiveData<String?>()
-
-    fun getArticle(Key: String, fetchImage: Boolean){
-        listArticle.postValue(null)
-        errorMessage.postValue(null)
-        val call = repository.getArticle(Key,fetchImage)
-        call.enqueue(object : Callback<ArticleResponse>{
+    fun getArticle(
+        key: String,
+        fetchImage: Boolean,
+        onSuccess: (List<ArticleResponse.ItemsItem>?) -> Unit,
+        onError: (String?) -> Unit
+    ) {
+        val call = repository.getArticle(key, fetchImage)
+        call.enqueue(object : Callback<ArticleResponse> {
             override fun onResponse(
                 call: Call<ArticleResponse>,
                 response: Response<ArticleResponse>
             ) {
                 if (response.isSuccessful)
-                    listArticle.postValue(response.body()?.items)
+                    onSuccess(response.body()?.items)
                 else
-                    errorMessage.postValue(response.errorBody().toString())
+                    onError(response.errorBody().toString())
             }
 
             override fun onFailure(call: Call<ArticleResponse>, t: Throwable) {
-                errorMessage.postValue(t.localizedMessage?.toString()?:t.message.toString())
+                onError(t.localizedMessage?.toString() ?: t.message.toString())
             }
         })
     }
