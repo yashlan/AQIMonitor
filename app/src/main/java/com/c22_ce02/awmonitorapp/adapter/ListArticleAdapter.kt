a358617ac2feef50c36f6ad64ceb0d949fa3c8f7
yashlan <yashlan007@gmail.com>
2022-06-07 20:48:02 +0700

    Update ListArticleAdapter.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/adapter/ListArticleAdapter.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/adapter/ListArticleAdapter.kt
@@ -7,7 +7,7 @@ import android.view.animation.AlphaAnimation
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.c22_ce02.awmonitorapp.data.model.Article
-import com.c22_ce02.awmonitorapp.databinding.ItemRowArticleBinding
+import com.c22_ce02.awmonitorapp.databinding.ItemRecycleviewArticleBinding
 import com.c22_ce02.awmonitorapp.ui.activity.DetailArticleActivity
 import com.c22_ce02.awmonitorapp.ui.fragment.ArticleFragment
 import com.c22_ce02.awmonitorapp.utils.loadImageViaGlide
@@ -16,19 +16,25 @@ class ListArticleAdapter(
     private val listArticle: ArrayList<Article>
 ) : RecyclerView.Adapter<ListArticleAdapter.ListViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val binding =
-            ItemRowArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ListViewHolder(binding)
-    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListViewHolder = ListViewHolder(
+        ItemRecycleviewArticleBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    )
 
-    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        holder.bind(listArticle[position])
-    }
+    override fun onBindViewHolder(
+        holder: ListViewHolder,
+        position: Int
+    ) = holder.bind(listArticle[position])
 
     override fun getItemCount(): Int = listArticle.size
 
-    inner class ListViewHolder(private var binding: ItemRowArticleBinding) :
+    inner class ListViewHolder(private val binding: ItemRecycleviewArticleBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(article: Article) {
             with(binding) {
@@ -37,7 +43,7 @@ class ListArticleAdapter(
                 tvItemCreatedBy.text = article.created_by
                 tvItemCreatedAt.text = article.created_at
                 itemView.apply {
-                    context.loadImageViaGlide(article.imageurl?.toUri(), binding.imgItemPhoto)
+                    context.loadImageViaGlide(article.imageurl?.toUri(), binding.imgThumbnail)
                     setOnClickListener {
                         startAnimation(AlphaAnimation(1f, 0.5f))
                         val i = Intent(context, DetailArticleActivity::class.java)
