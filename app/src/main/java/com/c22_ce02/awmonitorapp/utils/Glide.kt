0e994be4fc72e25103518277815b6ecb6561da31
yashlan <yashlan007@gmail.com>
2022-06-12 10:55:30 +0700

    Update Glide.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/Glide.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/Glide.kt
@@ -1,21 +1,17 @@
 package com.c22_ce02.awmonitorapp.utils
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 
 fun AppCompatActivity.loadImageViaGlide(uri: Uri?, target: ImageView) {
     Glide
         .with(this)
         .load(uri)
-        .centerCrop()
         .diskCacheStrategy(DiskCacheStrategy.NONE)
         .into(target)
 }
@@ -26,7 +22,6 @@ fun Context.loadImageViaGlide(uri: Uri?, target: ImageView) {
         .with(this)
         .load(uri)
         .centerCrop()
-        .override(200, 200)
         .diskCacheStrategy(DiskCacheStrategy.NONE)
         .into(target)
 }
@@ -35,7 +30,6 @@ fun AppCompatActivity.loadImageViaGlide(drawable: Int, target: ImageView) {
     Glide
         .with(this)
         .load(ContextCompat.getDrawable(this, drawable))
-        .centerCrop()
         .diskCacheStrategy(DiskCacheStrategy.NONE)
         .into(target)
 }
