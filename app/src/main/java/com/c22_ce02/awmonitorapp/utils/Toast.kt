60eb0c184f249e4e339e687cb1e5e85541f75f15
yashlan <yashlan007@gmail.com>
2022-06-13 13:40:13 +0700

    Update Toast.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/Toast.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/Toast.kt
@@ -1,6 +1,8 @@
 package com.c22_ce02.awmonitorapp.utils
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
@@ -28,4 +30,15 @@ fun Context.showToast(message: String) {
 
 fun Context.showToast(@StringRes resId: Int) {
     Toast.makeText(this, getString(resId), Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.showToastInThread(msg: String) {
+    Handler(Looper.getMainLooper()).post {
+        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+    }
+}
+fun Context.showToastInThread(msg: String) {
+    Handler(Looper.getMainLooper()).post {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
 }
