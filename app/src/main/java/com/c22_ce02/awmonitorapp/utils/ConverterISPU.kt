554cdb002df5ebee75113f24c342018be997b276
yashlan <yashlan007@gmail.com>
2022-06-16 13:16:59 +0700

    Update ConverterISPU.kt

--- app/src/main/java/com/c22_ce02/awmonitorapp/utils/ConverterISPU.kt
+++ app/src/main/java/com/c22_ce02/awmonitorapp/utils/ConverterISPU.kt
@@ -6,19 +6,19 @@ fun convertPM10ToISPU(pm10: Double): Int {
     val t: Double
     val r: Double
     when {
-        pm10 > 420.0 -> {
+        pm10.toInt() > 420 -> {
             k = pm10 - 420
             t = (500 - 420).toDouble()
             r = (400 - 300).toDouble()
             result = ((r * k / t) + 300).toInt()
         }
-        pm10 in 350.0..419.0 -> {
+        pm10.toInt() in 351..420 -> {
             k = pm10 - 350
             t = (420 - 350).toDouble()
             r = (300 - 200).toDouble()
             result = ((r * k / t) + 200).toInt()
         }
-        pm10 in 150.0..349.0 -> {
+        pm10.toInt() in 150..350 -> {
             k = pm10 - 150
             t = (350 - 150).toDouble()
             r = (200 - 100).toDouble()
@@ -40,19 +40,19 @@ fun convertPM25ToISPU(pm25: Double): Int {
     val t: Double
     val r: Double
     when {
-        pm25 > 250.4 -> {
+        pm25.toInt() > 250 -> {
             k = pm25 - 250.4
             t = 500 - 250.4
             r = (400 - 300).toDouble()
             result = ((r * k / t) + 300).toInt()
         }
-        pm25 in 150.4..249.4 -> {
+        pm25.toInt() in 150..250 -> {
             k = pm25 - 150.4
             t = 250.4 - 150.4
             r = (300 - 200).toDouble()
             result = ((r * k / t) + 200).toInt()
         }
-        pm25 in 55.4..149.4 -> {
+        pm25.toInt() in 55..149 -> {
             k = pm25 - 55.4
             t = 150.4 - 55.4
             r = (200 - 100).toDouble()
@@ -66,4 +66,152 @@ fun convertPM25ToISPU(pm25: Double): Int {
         }
     }
     return result
+}
+
+fun convertO3ToISPU(o3: Double): Int {
+    val result: Int
+    val k: Double
+    val t: Double
+    val r: Double
+    when {
+        o3.toInt() > 800 -> {
+            k = (o3 - 800)
+            t = ((1000 - 800).toDouble())
+            r = ((400 - 300).toDouble())
+            result = ((r * k / t) + 300).toInt()
+        }
+        o3.toInt() in 401..800 -> {
+            k = (o3 - 400)
+            t = ((800 - 400).toDouble())
+            r = ((300 - 200).toDouble())
+            result = ((r * k / t) + 200).toInt()
+        }
+        o3.toInt() in 236..400 -> {
+            k = (o3 - 235)
+            t = ((400 - 235).toDouble())
+            r = ((200 - 100).toDouble())
+            result = ((r * k / t) + 100).toInt()
+        }
+        else -> {
+            k = (o3 - 120)
+            t = ((235 - 120).toDouble())
+            r = ((100 - 50).toDouble())
+            result = ((r * k / t) + 50).toInt()
+        }
+    }
+    return result
+}
+
+fun convertSO2toISPU(so2: Double): Int {
+    val result: Int
+    val k: Double
+    val t: Double
+    val r: Double
+    when {
+        so2.toInt() > 800 -> {
+            k = so2 - 800
+            t = ((1200 - 800).toDouble())
+            r = ((400 - 300).toDouble())
+            result = ((r * k / t) + 300).toInt()
+        }
+        so2.toInt() in 401..800 -> {
+            k = (so2 - 400)
+            t = ((800 - 400).toDouble())
+            r = ((300 - 200).toDouble())
+            result = ((r * k / t) + 200).toInt()
+        }
+        so2.toInt() in 180..400 -> {
+            k = (so2 - 180)
+            t = ((400 - 180).toDouble())
+            r = ((200 - 100).toDouble())
+            result = ((r * k / t) + 100).toInt()
+        }
+        else -> {
+            k = (so2 - 52)
+            t = ((180 - 52).toDouble())
+            r = ((100 - 50).toDouble())
+            result = ((r * k / t) + 50).toInt()
+        }
+    }
+    return result
+}
+
+fun convertNO2toISPU(no2: Double): Int {
+    val result: Int
+    val k: Double
+    val t: Double
+    val r: Double
+    when {
+        no2.toInt() > 2260 -> {
+            k = no2 - 2260
+            t = ((3000 - 2260).toDouble())
+            r = ((400 - 300).toDouble())
+            result = ((r * k / t) + 300).toInt()
+        }
+        no2.toInt() in 1131..2260 -> {
+            k = (no2 - 1130)
+            t = ((2260 - 1130).toDouble())
+            r = ((300 - 200).toDouble())
+            result = ((r * k / t) + 200).toInt()
+        }
+        no2.toInt() in 200..1130 -> {
+            k = (no2 - 200)
+            t = ((1130 - 200).toDouble())
+            r = ((200 - 100).toDouble())
+            result = ((r * k / t) + 100).toInt()
+        }
+        else -> {
+            k = (no2 - 80)
+            t = ((235 - 80).toDouble())
+            r = ((100 - 50).toDouble())
+            result = ((r * k / t) + 50).toInt()
+        }
+    }
+    return result
+}
+
+fun convertCOtoISPU(co: Double): Int {
+    val result: Int
+    val k: Double
+    val t: Double
+    val r: Double
+    when {
+        co.toInt() > 30000 -> {
+            k = (co - 30000)
+            t = ((45000 - 30000).toDouble())
+            r = ((400 - 300).toDouble())
+            result = ((r * k / t) + 300).toInt()
+        }
+        co.toInt() in 15001..30000 -> {
+            k = (co - 15000)
+            t = ((30000 - 15000).toDouble())
+            r = ((300 - 200).toDouble())
+            result = ((r * k / t) + 200).toInt()
+        }
+        co.toInt() in 8000..15000 -> {
+            k = (co - 8000)
+            t = ((15000 - 8000).toDouble())
+            r = ((200 - 100).toDouble())
+            result = ((r * k / t) + 100).toInt()
+        }
+        else -> {
+            k = (co - 4000)
+            t = ((8000 - 4000).toDouble())
+            r = ((100 - 50).toDouble())
+            result = ((r * k / t) + 50).toInt()
+        }
+    }
+    return result
+}
+
+fun convertAQItoISPU(
+    pm10: Int,
+    pm25: Int,
+    o3: Int,
+    so2: Int,
+    no2: Int,
+    co: Int,
+): Int {
+    val list = listOf(pm10, pm25, o3, so2, no2, co)
+    return list.maxByOrNull { it } ?: 0
 }
